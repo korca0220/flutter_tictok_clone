899b3a51960579b9d08b24da68d4a332582eaada
ParkJuneWoo <krca0220@gmail.com>
2023-07-09 21:27:21 +0900

    #12.3 SliverGrid

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/sizes.dart';
+
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
 
@@ -10,41 +12,60 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          snap: true,
-          floating: true,
-          backgroundColor: Colors.teal,
-          elevation: 1,
-          collapsedHeight: 80,
-          expandedHeight: 200,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: const [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground,
-            ],
-            background: Image.asset(
-              'assets/images/placeholder.jpeg',
-              fit: BoxFit.cover,
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            backgroundColor: Colors.teal,
+            elevation: 1,
+            collapsedHeight: 80,
+            expandedHeight: 200,
+            flexibleSpace: FlexibleSpaceBar(
+              stretchModes: const [
+                StretchMode.blurBackground,
+                StretchMode.zoomBackground,
+              ],
+              background: Image.asset(
+                'assets/images/placeholder.jpeg',
+                fit: BoxFit.cover,
+              ),
+              title: const Text('Hello!'),
             ),
-            title: const Text('Hello!'),
           ),
-        ),
-        SliverFixedExtentList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text('Item $index'),
+          SliverFixedExtentList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 10,
+              (context, index) => Container(
+                color: Colors.amber[100 * (index % 9)],
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('Item $index'),
+                ),
               ),
             ),
+            itemExtent: 50,
           ),
-          itemExtent: 50,
-        ),
-      ],
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 20,
+              (context, index) => Container(
+                color: Colors.blue[100 * (index % 9)],
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('Item $index'),
+                ),
+              ),
+            ),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 100,
+              mainAxisSpacing: Sizes.size20,
+              crossAxisSpacing: Sizes.size20,
+              childAspectRatio: 1,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
