b83183be87c697eec617429737a0f24d6999a367
ParkJuneWoo <krca0220@gmail.com>
2023-07-19 21:40:49 +0900

    #15.3 google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ class TikTokApp extends StatelessWidget {
       title: 'TikTok Clone',
       themeMode: ThemeMode.system,
       darkTheme: ThemeData(
+        textTheme: GoogleFonts.rubikTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
         scaffoldBackgroundColor: Colors.black,
         primaryColor: const Color(0xFFe9435a),
         brightness: Brightness.dark,
@@ -27,67 +30,7 @@ class TikTokApp extends StatelessWidget {
         ),
       ),
       theme: ThemeData(
-        textTheme: TextTheme(
-          displayLarge: GoogleFonts.openSans(
-            fontSize: 95,
-            fontWeight: FontWeight.w300,
-            letterSpacing: -1.5,
-          ),
-          displayMedium: GoogleFonts.openSans(
-            fontSize: 59,
-            fontWeight: FontWeight.w300,
-            letterSpacing: -0.5,
-          ),
-          displaySmall:
-              GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
-          headlineMedium: GoogleFonts.openSans(
-            fontSize: 34,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.25,
-          ),
-          headlineSmall:
-              GoogleFonts.openSans(fontSize: 24, fontWeight: FontWeight.w400),
-          titleLarge: GoogleFonts.openSans(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 0.15,
-          ),
-          titleMedium: GoogleFonts.openSans(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.15,
-          ),
-          titleSmall: GoogleFonts.openSans(
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 0.1,
-          ),
-          bodyLarge: GoogleFonts.roboto(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.5,
-          ),
-          bodyMedium: GoogleFonts.roboto(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.25,
-          ),
-          labelLarge: GoogleFonts.roboto(
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 1.25,
-          ),
-          bodySmall: GoogleFonts.roboto(
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.4,
-          ),
-          labelSmall: GoogleFonts.roboto(
-            fontSize: 10,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 1.5,
-          ),
-        ),
+        textTheme: GoogleFonts.rubikTextTheme(),
         primaryColor: const Color(0xFFe9435a),
         scaffoldBackgroundColor: Colors.white,
         textSelectionTheme: const TextSelectionThemeData(
