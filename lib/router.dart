6847a9f44da5378c5f39d33898517e22ca8e59f8
ParkJuneWoo <krca0220@gmail.com>
2023-08-09 22:59:10 +0900

    #19.1 Installation camera

--- lib/router.dart
+++ lib/router.dart
@@ -1,35 +1,12 @@
 import 'package:go_router/go_router.dart';
 
-import 'features/authentication/email_screen.dart';
-import 'features/authentication/login_screen.dart';
-import 'features/authentication/sign_up_screen.dart';
-import 'features/authentication/username_screen.dart';
+import 'features/videos/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      path: SignUpScreen.routeURL,
-      name: SignUpScreen.routeName,
-      builder: (context, state) => const SignUpScreen(),
-      routes: [
-        GoRoute(
-          path: UsernameScreen.routeURL,
-          name: UsernameScreen.routeName,
-          builder: (context, state) => const UsernameScreen(),
-          routes: [
-            GoRoute(
-              path: EmailScreen.routeURL,
-              name: EmailScreen.routeName,
-              builder: (context, state) => const EmailScreen(),
-            ),
-          ],
-        ),
-      ],
-    ),
-    GoRoute(
-      path: LoginScreen.routeURL,
-      name: LoginScreen.routeName,
-      builder: (context, state) => const LoginScreen(),
+      path: '/',
+      builder: (context, state) => const VideoRecordingScreen(),
     ),
   ],
 );
